3d8d02a84d30633c0d94a116d8c6ec73540f783b
Benjamin Gaidos <33467420+GaidosBenjamin@users.noreply.github.com>
2020-04-07 21:55:44 +0300

    Add files via upload

---
+++ SortareLocalizata/Persoana.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SortareLocalizata
+{
+    class Persoana
+    {
+        private static string[] nume;
+
+        public Persoana(string fullName)
+        {
+            if (fullName.Length > 2)
+            {
+                nume = fullName.Split(' ');
+                for (int i = 0; i < nume.Length; i++)
+                {
+                    if (nume[i].Length > 1)
+                    {
+                        string var = nume[i];
+                        var = var.ToLower();
+                        var = char.ToUpper(var[0]) + var.Substring(1);
+                        nume[i] = var;
+                    }
+
+                }
+            }
+        }
+
+        public string getNume()
+        {
+            string rez = "";
+            foreach(string aux in nume)
+            {
+                rez = rez + aux + " ";
+            }
+            return rez;
+        }
+
+        public void printNume()
+        {
+            foreach(string var in nume)
+            {
+                Console.Write(var + " ");
+            }
+            Console.WriteLine();
+        }
+
+        public void setNume(string fullName)
+        {
+            nume = fullName.Split(' ');
+            for (int i = 0; i < nume.Length; i++)
+            {
+                string var = nume[i];
+                var.ToLower();
+                var = char.ToUpper(var[0]) + var.Substring(1);
+                nume[i] = var;
+            }
+        }
+    }
+}
---
+++ SortareLocalizata/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace SortareLocalizata
+{
+    class Program
+    {
+        private static List<string> lista = new List<string>();
+        static void Main(string[] args)
+        {
+            string[] lines = File.ReadAllLines(@"C:\Users\gaido\source\repos\SortareLocalizata\input.txt");
+           
+            foreach (string line in lines)
+            {
+                if(line.Length > 2)
+                {
+                    string[] aux = line.Split(' ');
+                    string nume = "";
+                    for(int i = 0; i < aux.Length; i++ )
+                    {
+                        if(aux[i].Length > 1)
+                        {
+                            aux[i] = aux[i].ToLower();
+                            aux[i] = char.ToUpper(aux[i][0]) + aux[i].Substring(1);
+                            nume = nume + aux[i] + " ";
+                        }
+                    }
+                    lista.Add(nume);
+                }
+
+            }
+            sort();
+            StreamWriter file = new StreamWriter(@"C:\Users\gaido\source\repos\SortareLocalizata\output.txt");
+            for (int i = 0; i < lista.Count; i++)
+            {
+                file.WriteLine(lista[i]);
+            }
+            file.Close();
+        }
+
+        public static void sort()
+        {
+            for(int i = 0; i < lista.Count; i++)
+            {
+                for(int j = 0; j < lista.Count; j++)
+                {
+                    if(String.Compare(lista[i], lista[j]) == -1)
+                    {
+                        string aux = lista[i];
+                        lista[i] = lista[j];
+                        lista[j] = aux;
+                    }
+                }
+            }
+        }
+    }
+}
